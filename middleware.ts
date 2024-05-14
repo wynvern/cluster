8985bd0e0ec4d3263b508b07149d79307955b2d2
wynvern <jeakarllo@gmail.com>
2024-05-13 21:25:07 -0300

    Beautiful Middleware Update

--- middleware.ts
+++ middleware.ts
@@ -1,58 +1,105 @@
 import { getToken } from "next-auth/jwt";
 import { type NextRequest, NextResponse } from "next/server";
 
-const PUBLIC_FILE = /\.[0-9a-z]+$/i;
+const excludedPrefixes = [
+	"/_next",
+	"/api",
+	"/static",
+	"/brand",
+	"/font",
+	"/service-worker",
+	"/manifest",
+	"/external",
+	"/login",
+	"/favicon",
+];
+
+interface RedirectStatement {
+	redirection: {
+		condition: boolean;
+		to: string;
+	};
+	location: string;
+}
 
 export default async function middleware(req: NextRequest) {
 	const url = new URL(req.url);
 	const session = await getToken({ req });
 
-	// New exclude system to handle evey single page
-	if (
-		url.pathname.startsWith("/_next") || // exclude Next.js internals
-		url.pathname.startsWith("/api") || //  exclude all API routes
-		url.pathname.startsWith("/static") || // exclude static files
-		PUBLIC_FILE.test(url.pathname) // exclude all files in the public folder
-	) {
+	if (excludedPrefixes.some((prefix) => url.pathname.startsWith(prefix)))
 		return NextResponse.next();
-	}
 
-	if (!session) {
-		if (
-			!url.pathname.includes("/signin") &&
-			!url.pathname.includes("/signup") &&
-			!url.pathname.includes("reset-password")
-		) {
-			return NextResponse.redirect(new URL("/signin", req.url));
-		}
-	} else {
-		if (!session.emailVerified && !url.pathname.includes("/verify-email")) {
-			return NextResponse.redirect(new URL("/verify-email", req.url));
-		}
-		if (session.emailVerified && url.pathname.includes("/verify-email")) {
-			return NextResponse.redirect(new URL("/", req.url));
-		}
-		if (
-			session.username &&
-			url.pathname.includes("/complete-profile") &&
-			session.emailVerified
-		) {
-			return NextResponse.redirect(new URL("/", req.url));
-		}
-		if (
-			url.pathname.includes("/login") ||
-			url.pathname.includes("/signup")
-		) {
-			return NextResponse.redirect(new URL("/", req.url));
-		}
-		if (
-			!session.username &&
-			!url.pathname.includes("/complete-profile") &&
-			session.emailVerified
-		) {
-			return NextResponse.redirect(new URL("/complete-profile", req.url));
-		}
-	}
+	// Mini API to handle redirects
+	// The route / means any route
+	const redirection: RedirectStatement[] = [
+		{
+			location: "/signin",
+			redirection: { condition: Boolean(session), to: "/" },
+		},
+		{
+			location: "/signup",
+			redirection: { condition: Boolean(session), to: "/" },
+		},
+		{
+			location: "/verify-email",
+			redirection: {
+				condition: Boolean(session) && Boolean(session?.emailVerified),
+				to: "/",
+			},
+		},
+		{
+			location: "/reset-password",
+			redirection: { condition: Boolean(session), to: "/" },
+		},
+		{
+			location: "/complete-profile",
+			redirection: {
+				condition: Boolean(session) && Boolean(session?.username),
+				to: "/",
+			},
+		},
+		{
+			location: "/",
+			redirection: {
+				condition:
+					!session &&
+					!url.pathname.includes("/signin") &&
+					!url.pathname.includes("/signup") &&
+					!url.pathname.includes("/reset-password"),
+				to: "/signin",
+			},
+		},
+		{
+			location: "/",
+			redirection: {
+				condition:
+					Boolean(session) &&
+					!session?.emailVerified &&
+					!url.pathname.includes("/verify-email"),
+				to: "/verify-email",
+			},
+		},
+		{
+			location: "/",
+			redirection: {
+				condition:
+					Boolean(session) &&
+					!session?.username &&
+					!url.pathname.includes("/complete-profile"),
+				to: "/complete-profile",
+			},
+		},
+	];
+
+	const abc = redirection.findIndex(
+		(rule) =>
+			rule.redirection.condition && url.pathname.startsWith(rule.location)
+	);
+
+	if (abc === -1) return NextResponse.next();
+	return NextResponse.redirect(
+		new URL(redirection[abc].redirection.to, req.url)
+	);
 }
 
 export const config = {
